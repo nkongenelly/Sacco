890796606ee39e9bb0691f32da58def88a48d8d7
kephamarasi <kephamarasi@gmail.com>
2018-10-22 13:56:12 +0300

    login page for admin

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,8 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
-
+        <link rel="stylesheet" href="/css/bootstrap/css/bootstrap.min.css" >
+        <link rel="stylesheet" href="/css/bootstrap/css/bootstrap.css" >
         <!-- Styles -->
         <style>
             html, body {
@@ -69,29 +70,18 @@
                 <div class="top-right links">
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
+                  
                     @endauth
                 </div>
             @endif
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Welcome to Kisborana Sacco system
                 </div>
+                <a class="btn btn-info btn-lg" href="{{ route('login') }}">Login</a>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+               
             </div>
         </div>
     </body>
